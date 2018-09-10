3b9265a1ed8a152c6d61418347bcf37582202ead
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 15:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function produceDrivingRange(blockRange) {
   return function(start,end) {
     const distance = (Math.abs(parseInt(end) - parseInt(start))
+    console.log(distance)
     debugger;
-    if (distance < blockRange) {
-      return `${blockRange - distance} blocks within range`
-    } else {return `${distance - blockRange} blocks out of range`}
-  }
+  //   if (distance < blockRange) {
+  //     return `${blockRange - distance} blocks within range`
+  //   } else {return `${distance - blockRange} blocks out of range`}
+  // }
 }
